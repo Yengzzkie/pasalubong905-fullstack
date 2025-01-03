e7f35b4fc003d0289a4f3addb4b2a5f00cf4f0b5
Yengzzkie <gatchalian.manuel@gmail.com>
2025-01-03 18:01:25 -0500

    removed WaterDropGrid component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,9 @@
 import OrdersCard from "../components/OrdersCard"
-import WaterDropGrid from "../components/ui/WaterDropGrid"
 
 const Dashboard = () => {
   return (
     <div>
       <OrdersCard />
-      {/* <WaterDropGrid /> */}
     </div>
   )
 }
